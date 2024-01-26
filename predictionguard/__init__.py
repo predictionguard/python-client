994788415ef9368795b6e69880e4c17e971e68dd
jmansdorfer <jmansdorfer19@gmail.com>
2024-01-26 09:01:02 -0500

    updating version number

--- predictionguard/__init__.py
+++ predictionguard/__init__.py
@@ -1,4 +1,4 @@
 """Create controlled and compliant AI systems with PredictionGuard."""
 from .client import Client, Completion, Chat, Translate, Factuality, Toxicity, PII, Injection
 
-__version__ = "2.0.0"
+__version__ = "1.9.0"
