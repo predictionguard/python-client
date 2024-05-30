3049265e49c7c62221b90faa78474431181845cc
Jacob Mansdorfer <90076431+jmansdorfer@users.noreply.github.com>
2024-05-30 13:05:36 -0400

    Adding vision support. (#20)
    * updating chat model list
    
    * initial vision commit
    
    * working vision functionality

--- client_test.py
+++ client_test.py
@@ -59,7 +59,6 @@ Please contact support.
             api_key=os.environ["PREDICTIONGUARD_API_KEY"],
             url="https://www.predictionguard.com/"
             )
-        
 
 #-------------------------#
 #    Completions Tests    #
@@ -148,6 +147,151 @@ def test_chat_completions_create_stream():
         response_list.append(res)
 
     assert len(response_list) > 1
+
+
+def test_chat_completions_create_stream_output_fail():
+    test_client = PredictionGuard()
+
+    streaming_error = "Factuality and toxicity checks are not supported when streaming is enabled.".replace("\n", "")
+
+    response_list = []
+    with pytest.raises(ValueError, match=streaming_error):
+        for res in test_client.chat.completions.create(
+            model=os.environ["TEST_MODEL_NAME"],
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful chatbot."
+                },
+                {
+                    "role": "user",
+                    "content": "Tell me a joke."
+                }
+            ],
+            stream=True,
+            output={
+                "toxicity": True
+            }
+        ):
+            response_list.append(res)
+
+
+def test_chat_completions_create_vision_image_file():
+    test_client = PredictionGuard()
+
+    response = test_client.chat.completions.create(
+        model=os.environ["TEST_VISION_MODEL"],
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What is in this image?"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "fixtures/test_image.jpeg"
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+
+    assert response["choices"][0]["status"] == "success"
+    assert len(response["choices"][0]["message"]["content"]) > 0
+
+
+def test_chat_completions_create_vision_image_url():
+    test_client = PredictionGuard()
+
+    response = test_client.chat.completions.create(
+        model=os.environ["TEST_VISION_MODEL"],
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What is in this image?"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://farm4.staticflickr.com/3300/3497460990_11dfb95dd1_z.jpg"
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+
+    assert response["choices"][0]["status"] == "success"
+    assert len(response["choices"][0]["message"]["content"]) > 0
+
+
+def test_chat_completions_create_vision_image_b64():
+    test_client = PredictionGuard()
+
+    with open("fixtures/test_image.jpeg", "rb") as image_file:
+            b64_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+    response = test_client.chat.completions.create(
+        model=os.environ["TEST_VISION_MODEL"],
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What is in this image?"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": b64_image
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+
+    assert response["choices"][0]["status"] == "success"
+    assert len(response["choices"][0]["message"]["content"]) > 0
+
+
+def test_chat_completions_create_vision_stream_fail():
+    test_client = PredictionGuard()
+
+    streaming_error = "Streaming is not currently supported when using vision."
+
+    response_list = []
+    with pytest.raises(ValueError, match=streaming_error):
+        for res in test_client.chat.completions.create(
+            model=os.environ["TEST_VISION_MODEL"],
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "What is in this image?"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": "fixtures/test_image.jpeg"
+                            }
+                        }
+                    ]
+                }
+            ],
+            stream=True
+        ):
+            response_list.append(res)
 
 
 def test_chat_completions_list_models():
