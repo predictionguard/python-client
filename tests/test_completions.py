66f9e8a0ac65ab637db76e845057607eaad88100
jmansdorfer <jmansdorfer19@gmail.com>
2025-01-28 14:31:21 -0500

    initial vllm work

--- tests/test_completions.py
+++ tests/test_completions.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 from predictionguard import PredictionGuard
 
 
@@ -33,3 +35,35 @@ def test_completions_list_models():
 
     assert len(response) > 0
     assert type(response[0]) is str
+
+
+def test_completions_create_stream():
+    test_client = PredictionGuard()
+
+    response_list = []
+    for res in test_client.completions.create(
+        model=os.environ["TEST_MODEL_NAME"],
+        prompt="Tell me a joke.",
+        stream=True,
+    ):
+        response_list.append(res)
+
+    assert len(response_list) > 1
+
+
+def test_completions_create_stream_output_fail():
+    test_client = PredictionGuard()
+
+    streaming_error = "Factuality and toxicity checks are not supported when streaming is enabled.".replace(
+        "\n", ""
+    )
+
+    response_list = []
+    with pytest.raises(ValueError, match=streaming_error):
+        for res in test_client.completions.create(
+            model=os.environ["TEST_MODEL_NAME"],
+            prompt="Tell me a joke.",
+            stream=True,
+            output={"toxicity": True},
+        ):
+            response_list.append(res)
