905dec614de7a7fbb1e77b1794e2aaa34505e00d
jmansdorfer <jmansdorfer19@gmail.com>
2025-11-25 11:24:20 -0500

    commenting out tests

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -11,47 +11,48 @@ def test_audio_transcribe_success():
 
     assert len(response["text"]) > 0
 
-def test_audio_transcribe_timestamps_success():
-    test_client = PredictionGuard()
-
-    response = test_client.audio.transcriptions.create(
-        model="base",
-        file="fixtures/test_audio.wav",
-        timestamp_granularities=["word", "segment"],
-        response_format="verbose_json"
-    )
-
-    assert len(response["text"]) > 0
-    assert len(response["segments"]) > 0
-    assert len(response["segments"][0]["text"]) > 0
-    assert len(response["words"]) > 0
-    assert len(response["words"][0]["text"]) > 0
-
-def test_audio_transcribe_diarization_success():
-    test_client = PredictionGuard()
-
-    response = test_client.audio.transcriptions.create(
-        model="base",
-        file="fixtures/test_audio.wav",
-        diarization=True,
-        response_format="verbose_json"
-    )
-
-    assert len(response["text"]) > 0
-    assert len(response["segments"]) > 0
-    assert len(response["segments"][0]["text"]) > 0
-
-def test_audio_transcribe_diarization_timestamps_success():
-    test_client = PredictionGuard()
-
-    response = test_client.audio.transcriptions.create(
-        model="base",
-        file="fixtures/test_audio.wav",
-        diarization=True,
-        timestamp_granularities=["word"],
-        response_format="verbose_json"
-    )
-
-    assert len(response["text"]) > 0
-    assert len(response["words"]) > 0
-    assert len(response["words"][0]["text"]) > 0
+# Uncomment when in production
+# def test_audio_transcribe_timestamps_success():
+#     test_client = PredictionGuard()
+#
+#     response = test_client.audio.transcriptions.create(
+#         model="base",
+#         file="fixtures/test_audio.wav",
+#         timestamp_granularities=["word", "segment"],
+#         response_format="verbose_json"
+#     )
+#
+#     assert len(response["text"]) > 0
+#     assert len(response["segments"]) > 0
+#     assert len(response["segments"][0]["text"]) > 0
+#     assert len(response["words"]) > 0
+#     assert len(response["words"][0]["text"]) > 0
+#
+# def test_audio_transcribe_diarization_success():
+#     test_client = PredictionGuard()
+#
+#     response = test_client.audio.transcriptions.create(
+#         model="base",
+#         file="fixtures/test_audio.wav",
+#         diarization=True,
+#         response_format="verbose_json"
+#     )
+#
+#     assert len(response["text"]) > 0
+#     assert len(response["segments"]) > 0
+#     assert len(response["segments"][0]["text"]) > 0
+#
+# def test_audio_transcribe_diarization_timestamps_success():
+#     test_client = PredictionGuard()
+#
+#     response = test_client.audio.transcriptions.create(
+#         model="base",
+#         file="fixtures/test_audio.wav",
+#         diarization=True,
+#         timestamp_granularities=["word"],
+#         response_format="verbose_json"
+#     )
+#
+#     assert len(response["text"]) > 0
+#     assert len(response["words"]) > 0
+#     assert len(response["words"][0]["text"]) > 0
